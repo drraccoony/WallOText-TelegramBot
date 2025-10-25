c2e89842e4a0d91df848e5462f3dd7686935b565
Shawn McHenry <drraccoony@gmail.com>
2025-10-25 12:05:24 -0500

    Base route

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,18 @@
 
 use App\Http\Controllers\TelegramWebhookController;
 use Illuminate\Support\Facades\Route;
+use App\Models\WallStreak;
+
+Route::get('/', function () {
+    $streak = WallStreak::first(); // assuming only 1 target user is tracked
+
+    return response()->json([
+        'status' => 'online',
+        'last_wall_timestamp' => $streak?->last_wall_at?->toIso8601String(),
+        'last_wall_readable' => $streak?->last_wall_at
+            ? $streak->last_wall_at->format('F j, Y g:i A')
+            : null,
+    ]);
+});
 
 Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
